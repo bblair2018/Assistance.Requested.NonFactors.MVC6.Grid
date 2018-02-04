6299ff87d21e29913984c57eab478a04c0969f35
bblair2018 <john.w.blair@gmail.com>
2018-02-04 14:09:18 -0600

    Checking this into git for the firsttime!

---
+++ loadingpanel/Controllers/HouseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace loadingpanel.Controllers
+{
+    public class HouseController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index(string message)
+        {
+            Thread.Sleep(3000);
+            //ViewData["Message"] = "Your *REAL* email has been sent!!!!!!";
+            ViewData["Message"] = message;
+            return View();
+        }
+    }
+}
---
+++ loadingpanel/Data/IEmailSenderRepository.cs
@@ -0,0 +1,14 @@
+﻿using loadingpanel.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadingpanel.Data
+{
+    public interface IEmailSenderRepository
+    {
+        IEnumerable<EmailItem> GetAllEmailItems();
+    }
+}
