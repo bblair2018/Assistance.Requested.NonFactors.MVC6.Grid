b7e52155d3f13f8a7646625a669c8fa1dc7c2ed7
bblair2018 <john.w.blair@gmail.com>
2018-02-04 14:16:48 -0600

    Doing my first checkin!

---
+++ loadingpanel/Controllers/CondoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadingpanel.Controllers
+{
+    public class CondoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
